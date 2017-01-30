eaa75656f79487f21b08c3ff67bc8c822b77e4d9
Elisa Danthinne <danthinne.e@husky.neu.edu>
2017-01-30 16:17:13 -0500

    ran cpplint to change style format

--- include/MergeSort.h
+++ include/MergeSort.h
@@ -1,12 +1,12 @@
 // what are these ???
-#ifndef MERGE_SORT_H
-#define MERGE_SORT_H
+#ifndef MERGE_SORT_H_
+#define MERGE_SORT_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 
-//only declare functions to be tested, used outside of cpp file, etc.
-void MergeSort(int *A,int n);
+// only declare functions to be tested, used outside of cpp file, etc.
+void MergeSort(int *A, int n);
 
-#endif
+#endif  // INCLUDE MERGE_SORT_H_
