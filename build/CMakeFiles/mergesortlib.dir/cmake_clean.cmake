3a3fd1d3b8bc7956a1fa52e053d37a5b77c69c9f
Elisa Danthinne <danthinne.e@husky.neu.edu>
2017-01-30 15:46:14 -0500

    testing gitignore

---
+++ build/CMakeFiles/mergesortlib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mergesortlib.dir/src/MergeSort.cpp.o"
+  "libmergesortlib.pdb"
+  "libmergesortlib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mergesortlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
